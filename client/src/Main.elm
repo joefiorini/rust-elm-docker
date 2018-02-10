6df8dc4087b16d6fa5e04e3ff0010f767691624b
Joe Fiorini <joe@joefiorini.com>
2018-02-10 16:00:27 -0500

    Working client/server interaction

---
+++ client/src/Main.elm
@@ -0,0 +1,110 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+
+
+logger : (Msg -> Model -> ( Model, Cmd Msg )) -> Msg -> Model -> ( Model, Cmd Msg )
+logger update msg model =
+    let
+        originalModel =
+            Debug.log "Before:" model
+
+        originalMsg =
+            Debug.log "Msg: " msg
+
+        ( afterModel, afterCmd ) =
+            update originalMsg originalModel
+    in
+        ( Debug.log "After:" afterModel, afterCmd )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String, greeting : Maybe String, error : Maybe String }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SetName String
+    | RequestGreeting
+    | SetGreeting (Result Http.Error String)
+    | Reset
+
+
+main : Program Never Model Msg
+main =
+    Html.program { init = init, view = view, update = logger update, subscriptions = subscriptions }
+
+
+init : ( Model, Cmd Msg )
+init =
+    { name = ""
+    , greeting = Nothing
+    , error = Nothing
+    }
+        ! []
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+getGreeting : String -> Cmd Msg
+getGreeting name =
+    "http://localhost:8000/hello/"
+        ++ name
+        |> Http.getString
+        |> Http.send SetGreeting
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Reset ->
+            { model | name = "" } ! []
+
+        SetName name ->
+            { model | name = name } ! []
+
+        RequestGreeting ->
+            model ! [ getGreeting model.name ]
+
+        SetGreeting r ->
+            case r of
+                Ok greeting ->
+                    { model | greeting = Just greeting } ! []
+
+                Err error ->
+                    { model | error = Just "Could not get greeting", greeting = Nothing } ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    section []
+        [ input
+            [ type_ "text", value model.name, onInput SetName ]
+            []
+        , button [ onClick RequestGreeting ] [ text "Get Greeting" ]
+        , greetingView model.greeting
+        ]
+
+
+greetingView : Maybe String -> Html Msg
+greetingView greeting =
+    greeting
+        |> Maybe.map (\g -> section [] [ text g ])
+        |> Maybe.withDefault (section [] [])
---
+++ server/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate rocket_cors;
+
+use rocket::http::Method;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+
+#[get("/hello/<name>")]
+fn hello_name(name: String) -> String {
+    format!("Hello {}!", name)
+}
+
+fn main() {
+    let (allowed_origins, failed_origins) = AllowedOrigins::some(&["http://localhost:8001"]);
+    assert!(failed_origins.is_empty());
+
+    let options = rocket_cors::Cors {
+        allowed_origins: allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    };
+
+    rocket::ignite()
+        .mount("/", routes![hello_name])
+        .attach(options)
+        .launch();
+}
